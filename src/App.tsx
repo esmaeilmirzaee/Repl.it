8c1156eebcbda2123d1093577413c625caab3e54
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2022-01-05 09:07:56 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function App() {
+  return (
+    <main className='flex h-screen w-screen justify-center items-center text-zinc-800'>
+      React⚛️ + Vite⚡ + Replit🌀
+    </main>
+  )
+}
